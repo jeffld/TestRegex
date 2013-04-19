b5379f5828a12a82da1ca2c58ec57f1c9009dbec
Jeff Darling <jeffld3@gmail.com>
2013-04-19 13:49:00 -0500

    Initial Commit

---
+++ TestRegex/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TestRegex
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.txtMatch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(15, 12);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(86, 34);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(111, 86);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(308, 20);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.Text = "Order.20250624.ItemData.20152334.xml";
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(111, 28);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(308, 20);
+            this.txtRegex.TabIndex = 3;
+            this.txtRegex.Text = "^Order\\.(\\d{1,})\\.ItemData\\..*\\.xml$";
+            // 
+            // txtMatch
+            // 
+            this.txtMatch.Location = new System.Drawing.Point(111, 142);
+            this.txtMatch.Multiline = true;
+            this.txtMatch.Name = "txtMatch";
+            this.txtMatch.Size = new System.Drawing.Size(308, 93);
+            this.txtMatch.TabIndex = 4;
+            this.txtMatch.TextChanged += new System.EventHandler(this.txtMatch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(111, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Regular Expression";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(111, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Source String";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(111, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Matches";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(510, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMatch);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "frmMain";
+            this.Text = "Regex Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.TextBox txtMatch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TestRegex/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace TestRegex
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Define a regular expression
+            try
+            {
+                Regex rx = new Regex(txtRegex.Text, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+
+                // Find matches.
+                MatchCollection matches = rx.Matches(txtSource.Text);
+
+                txtMatch.Text = "";
+
+                foreach (Match match in matches)
+                {
+                    // Report the number of matches found.
+                    //Console.WriteLine("{0} matches found in: {1} value: {2}\n", matches.Count, txtSource.Text, match.Groups[1].ToString());
+
+                    //txtMatch.Text = match.Groups[1].ToString();
+
+                    
+
+                    for (int i = 1; i < match.Groups.Count; i++)
+                    {
+                        var group = match.Groups[i];
+                        if (group.Success)
+                            txtMatch.Text = txtMatch.Text + "Group: " + rx.GroupNameFromNumber(i) + "  Value: " + match.Groups[i].ToString() + "\r\n";
+
+                    }
+
+                }
+            }
+            catch(Exception x)
+            {
+                // Catch the exception and display the error message.
+                MessageBox.Show(x.Message, "Oops.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+
+
+        }
+
+
+
+        private void txtMatch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
